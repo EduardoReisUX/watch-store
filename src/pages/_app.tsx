6e59edd309ece5fb264212fa5fc7a0f22c9bb974
Eduardo Fernandes <ducabruno09@hotmail.com>
2023-01-09 11:19:26 -0400

    feat: add Layout component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { makeServer } from "../services/miragejs/server";
+import { Layout } from "../components/Layout";
 
 if (process.env.NODE_ENV === "development") {
   makeServer();
 }
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
